c45ae0d2b97c7b8a7472e3653915272224c7436d
AramisAra <martinezcruzaj@gmail.com>
2024-09-04 15:15:56 -0400

    fixing pep

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,21 +3,23 @@
 Determine if all the boxes in the given list can be unlocked.
 
 Args:
-  boxes (list): A list of boxes, where each box is represented as a list of keys.
+    boxes (list): A list of boxes, where each box is
+    represented as a list of keys.
 
 Returns:
   bool: True if all the boxes can be unlocked, False otherwise.
 """
 
+
 def canUnlockAll(boxes: list) -> bool:
-  boxeslen: int = len(boxes)
-  unlocked: list = [False] * boxeslen
-  unlocked[0] = True
-  stack: list = [0]
-  while stack:
-    current_box = stack.pop()
+    boxeslen: int = len(boxes)
+    unlocked: list = [False] * boxeslen
+    unlocked[0] = True
+    stack: list = [0]
+    while stack:
+        current_box = stack.pop()
     for key in boxes[current_box]:
-      if key < boxeslen and not unlocked[key]:
-        unlocked[key] = True
-        stack.append(key)
-  return all(unlocked)
+        if key < boxeslen and not unlocked[key]:
+            unlocked[key] = True
+            stack.append(key)
+    return all(unlocked)
