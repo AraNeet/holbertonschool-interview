ba1e50cb87188e6c5ce6e726eb7077e8781e6963
AramisAra <martinezcruzaj@gmail.com>
2024-09-04 15:11:27 -0400

    What do you want pep

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,20 +8,16 @@ Args:
 Returns:
   bool: True if all the boxes can be unlocked, False otherwise.
 """
+
 def canUnlockAll(boxes: list) -> bool:
   boxeslen: int = len(boxes)
-
   unlocked: list = [False] * boxeslen
   unlocked[0] = True
-
   stack: list = [0]
-
   while stack:
     current_box = stack.pop()
-
     for key in boxes[current_box]:
       if key < boxeslen and not unlocked[key]:
         unlocked[key] = True
         stack.append(key)
-
   return all(unlocked)
