d2c4487797f71a231986f3b06ec97c0039fe87f7
araneet <martinezcruzaj@gmail.com>
2025-04-02 14:42:26 -0400

    Adding doc

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * check_cycle - Check of the linked list has a loop
+ *
+ * @list: The list give to check.
+ *
+ * Return: if looped 1 if not 0
+ */
 int check_cycle(listint_t *list) {
   listint_t *Turtle = list;
   listint_t *Hare = list;
